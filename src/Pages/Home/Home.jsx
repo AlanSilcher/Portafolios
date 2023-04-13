91f32ad32e36ccae5b5b7bbe04a3918e1b1a3950
AlanSilcher <silcheralan@gmail.com>
2023-04-12 23:12:27 -0300

    Intento 2.2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
 
 export default function Home() { 
     return (
         <div>
             <h1>Esto es home</h1>
             <h3>Aca va el resumen personal</h3>
+            <Link to= '/reviews'> <button>Reviews</button></Link>
+            <Link to= '/contact'> <button>Contact</button></Link>
+            <Link to= '/projects'> <button>Projects</button></Link>
         </div>
     )
 }
