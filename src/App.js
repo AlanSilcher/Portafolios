f8890c56c9554d003b5a4f2d6f5f6605b7edccc6
AlanSilcher <silcheralan@gmail.com>
2023-04-27 12:01:18 -0300

    Ajuste

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Me from './Pages/Me/Me';
 import Projects from './Pages/Projects/Projects';
 import Solutions from "./Pages/Solutions/Solutions"
 import Technologies from './Pages/Technologies/Technologies';
-import Achievements from './Pages/Achievements/Achievements';
 import Certifications from './Pages/Certifications/Certifications';
 import Contact from './Pages/Contact/Contact';
 import Reviews from './Pages/Reviews/Reviews';
@@ -19,7 +18,6 @@ function App() {
       <Route path="/solutions"><Solutions/></Route>
       <Route path="/projects"><Projects/></Route>
       <Route path="/technologies"><Technologies/></Route>
-      <Route path="/achievements"><Achievements/></Route>
       <Route path="/certifications"><Certifications/></Route>
       <Route path="/contact"><Contact/></Route>
       <Route path="/reviews"><Reviews/></Route>
