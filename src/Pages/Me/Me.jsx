d6dba186a2cf5bb8f24b0490a960fb6f2b9a1e6e
AlanSilcher <silcheralan@gmail.com>
2023-05-02 18:35:15 -0300

    Dale

--- src/Pages/Me/Me.jsx
+++ src/Pages/Me/Me.jsx
@@ -23,7 +23,7 @@ export default function Me() {
                     <div class="cards-list">
   
                     <div class="card 1">
-                    <div class="card_image"> <img src="https://www.colorhexa.com/262624.png" alt="me photo"/> </div>
+                    <div class="card_image"> <img src="https://www.colorhexa.com/262624.png" alt="foto mia"/> </div>
                     <div class="card_title title-white">
                     <Icon path={mdiMedal} size={2} />
                     <p>Experiencia</p>
